69e4831ee874aebe3e3c35ec4d9b8432bca065fe
Artur Gajewski <artur.gajewski@fraktio.fi>
2019-06-05 10:54:41 +0300

    CSS changes

--- src/services/news.js
+++ src/services/news.js
@@ -4,6 +4,11 @@ const { REACT_APP_API_URL } = process.env;
 
 export default {
   getNews: async () => {
-    return axios.get(`${REACT_APP_API_URL}/news`).then(r => r.data);
+    return axios
+      .get(`${REACT_APP_API_URL}/news`)
+      .then(r => r.data)
+      .catch(function(error) {
+        return null;
+      });
   }
 };
